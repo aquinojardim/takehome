f6361e8200f081d481dcdc7292a32636d5415c6b
aquinojardim <64516365+aquinojardim@users.noreply.github.com>
2020-08-13 17:47:02 -0700

    corrected routes

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -4,12 +4,12 @@ const myController = require('../controllers/myController');
 const router = express.Router();
 
 // serve the /info API listening to get requests, retrive the info from database and send as a response to the front-end
-router.get('/', myController.getInfo, (req, res) => {
+router.get('/info', myController.getInfo, (req, res) => {
   res.status(200).json({...res.locals.data});
 })
 
 // serve the /info API listening to post requests, retrive the info from front-end, save it to database and send as a response to the front-end with the id created by the database
-router.post('/', myController.postInfo, (req, res) => {
+router.post('/info', myController.postInfo, (req, res) => {
   res.status(200).json({...res.locals.data});
 })
 
